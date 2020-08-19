ff2cf4bd93994e0b7590e6f8a54936829bc86fb8
live00free <17611119758@163.com>
2020-08-19 22:54:38 +0800

    vue

--- nodejs/01-helloword.js
+++ nodejs/01-helloword.js
@@ -5,6 +5,7 @@ let person={
 const getAge=user=>user.age;
 let call=`Hello,${person.name},你今年是岁${getAge(person)}吗？`;
 console.log(call);
+console.log(call+"是不是？");
 
 //exports.xiaoming=person;
 //exports.getUserAg=getAge;
